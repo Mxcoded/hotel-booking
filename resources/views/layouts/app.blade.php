78e92f48154fd7e89e391b0715df7ad2f101d9ab
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2025-08-28 18:45:05 +0100

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/' . setting('favicon')) }}">
     <!-- SEO Meta Tags -->
     <title>@yield('title', 'Brickspoint Boutique Aparthotel - Wuse II, Abuja')</title>
     <meta name="description"
